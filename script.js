b5a4e6e42c22ad8f345440525a371618ac660300
Mauro S S Goncalves <133293974+mrmssg@users.noreply.github.com>
2025-03-25 17:18:26 +0000

    007

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
+botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
