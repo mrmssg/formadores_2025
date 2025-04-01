4386b8be6e9412221eef54c2f83d8e32c658e928
Mauro S S Goncalves <133293974+mrmssg@users.noreply.github.com>
2025-04-01 17:56:33 +0000

    Alterado javascript para ativar contraste

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
 
     let tamanhoAtualFonte = 1;
 
@@ -24,6 +25,9 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
 })
 
 
